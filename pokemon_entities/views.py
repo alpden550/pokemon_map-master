4cf92fae6812def89e51e130298fa7e1a978847a
alpden550 <alpden550@gmail.com>
2019-10-04 21:16:23 +0300

    Reduced requests to db.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-from .models import Pokemon, PokemonEntity
+from .models import Pokemon
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
@@ -16,8 +16,12 @@ def add_pokemon(folium_map, lat, lon, name, image_url=DEFAULT_IMAGE_URL):
 
 def show_all_pokemons(request):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+    all_pokemons = Pokemon.objects.prefetch_related('pokemon_entity').all()
+    all_entities = []
+    for pokemon in all_pokemons:
+        all_entities.extend(pokemon.pokemon_entity.all())
 
-    for pokemon in PokemonEntity.objects.all():
+    for pokemon in all_entities:
         photo_url = (
             request.build_absolute_uri(pokemon.pokemon.photo.url)
             if pokemon.pokemon.photo
@@ -26,9 +30,9 @@ def show_all_pokemons(request):
         if not photo_url:
             continue
         add_pokemon(folium_map, pokemon.lat, pokemon.lon, pokemon.pokemon, photo_url)
-    pokemons_on_page = []
 
-    for pokemon in Pokemon.objects.all():
+    pokemons_on_page = []
+    for pokemon in all_pokemons:
         pokemons_on_page.append(
             {
                 'pokemon_id': pokemon.id,
@@ -46,7 +50,7 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     try:
-        pokemon_class = Pokemon.objects.get(id=pokemon_id)
+        pokemon_class = Pokemon.objects.prefetch_related('pokemon_entity').get(id=pokemon_id)
     except ObjectDoesNotExist:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
@@ -80,7 +84,8 @@ def show_pokemon(request, pokemon_id):
     }
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    pokemon_entities = PokemonEntity.objects.filter(pokemon=pokemon_class)
+
+    pokemon_entities = pokemon_class.pokemon_entity.all()
     for pokemon_entity in pokemon_entities:
         photo_url = (
             request.build_absolute_uri(pokemon_entity.pokemon.photo.url)
