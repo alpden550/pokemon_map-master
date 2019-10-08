40273d0633507b4abdabed40c6d25861aad791ca
alpden550 <alpden550@gmail.com>
2019-10-08 19:45:05 +0300

    Added pokemon's type

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class PokemonElementType(models.Model):
+    name = models.CharField('Стихия', max_length=120)
+    avatar = models.ImageField('Аватар стихии', upload_to='type', null=True, blank=True)
+
+    def __str__(self):
+        return f'Element {self.name}'
+
+
 class Pokemon(models.Model):
     title = models.CharField('Покемон', max_length=200)
     title_en = models.CharField(
@@ -26,6 +34,8 @@ class Pokemon(models.Model):
         on_delete=models.CASCADE,
         related_name='next_evolutions',
     )
+    elements = models.ManyToManyField(
+        PokemonElementType, verbose_name="Стихии", related_name='pokemons', blank=True)
     photo = models.ImageField('Фото', blank=True, null=True)
 
     def __str__(self):
